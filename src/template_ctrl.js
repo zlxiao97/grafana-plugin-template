ab65f8a4839a1c0de51b6fce330c783925a6bd47
2552895864 <2552895864@qq.com>
2020-02-05 17:01:25 +0800

    add Config/fix class bug

--- src/template_ctrl.js
+++ src/template_ctrl.js
@@ -1,13 +1,26 @@
 import { PanelCtrl } from 'grafana/app/plugins/sdk';
 import moment from 'moment';
+import _ from 'lodash';
 import './css/panel.css';
+
+const panelDefaults = {
+  fontsize: 12,
+  bgColor: null
+};
 
 export class TemplateCtrl extends PanelCtrl {
   static templateUrl = 'partials/module.html';
+  static scrollable = true;
 
   constructor($scope, $injector) {
     super($scope, $injector);
+    _.defaults(this.panel, panelDefaults);
+    this.events.on('init-edit-mode', this.onInitEditMode.bind(this));
     this.updateClock();
+  }
+
+  onInitEditMode(){
+    this.addEditorTab('Options', 'public/plugins/grafana-template-panel/partials/options.html', 2);
   }
 
   updateClock() {
